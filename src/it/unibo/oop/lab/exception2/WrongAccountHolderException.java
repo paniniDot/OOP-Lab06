042ce00daf375b1532ed9dc59fd4c330e4db502b
Mattia Panni <mattia.panni@studio.unibo.it>
2021-11-06 16:04:46 +0100

    Implement WrongAccountHolderException

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -2,4 +2,25 @@ package it.unibo.oop.lab.exception2;
 
 public class WrongAccountHolderException extends IllegalStateException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7190620326792084901L;
+
+	private final int wrongUserID;
+	
+	public WrongAccountHolderException(final int ID) {
+		super();
+		this.wrongUserID = ID;
+	}
+	
+	@Override
+	public String toString() {
+		return "Entered a wrong user ID: " + this.wrongUserID; 
+	}
+	
+	@Override public String getMessage() {
+		return this.toString();
+	}
+	
 }
